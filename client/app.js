d0e8059c974db52dca794ddc4c5a57044dda8ba2
Mike Luby <Michael@mLuby.com>
2014-10-28 22:46:51 -0700

    local file system connected to client

--- client/app.js
+++ client/app.js
@@ -1,7 +1,45 @@
 var app = angular.module('dynamicEmail',[]);
 
-app.controller('composeEmail', function($scope){
-  $scope.submitEmail = function(body){ 
-    alert(body); 
+app.controller('composeEmail', function($scope, http){
+  $scope.submitEmail = function(emailBody){ 
+    console.log('getting image', emailBody);
+    http.createImage(emailBody).then(function(response){
+      console.log('response',response.data);
+    }); 
   };
+});
+
+app.factory('http', function($http){
+  var getImage = function(imageID){
+    return $http({
+      url: '/api/images/'+imageID,
+      method: 'GET'
+    }).success(function(response){
+      return response;
+    });
+  };
+
+  var createImage = function(emailBody){
+    return $http({
+      url: '/api/create/'+Math.floor(Math.random()*100000000),
+      method: 'POST',
+      dataType: 'application/json',
+      data: {emailBody: emailBody}
+    }).success(function(response){
+      return response;
+    });
+  };
+
+  var createImage2 = function(emailBody){
+    return $http.post('/api/create/'+Math.floor(Math.random()*100000000),
+      {'emailBody': emailBody}
+    ).success(function(response){
+      return response;
+    });
+  };
+
+  return {
+    getImage: getImage,
+    createImage: createImage2
+  }
 });
