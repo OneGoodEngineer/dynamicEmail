470b24ccc5048b0e2d77437394cd6bd82399b6c4
Mike Luby <Michael@mLuby.com>
2014-11-04 23:45:47 -0800

    progress toward working image urls

--- server.js
+++ server.js
@@ -1,59 +1,56 @@
 var express = require('express');
-var fs = require('fs');
-var path = require('path');
-var bodyParser = require('body-parser')
+var http = require('http');
 
 var app = express();
 
-app.use(bodyParser.json()); // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded());
 app.use(express.static(__dirname)); // serve static client
 app.get('/', function(req,res){ // get to static client
   res.redirect('/client/');
 });
 
-app.post('/api/create/:imgid', function(req, res){
-  var pathToImage = path.join(__dirname, '/images/', req.params.imgid);
-  console.log('image created at',pathToImage);
-  var data = req.body.emailBody;
-
-  function decodeBase64Image(dataString) {
-    var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-      response = {};
-
-    if (matches.length !== 3) {
-      return new Error('Invalid input string');
+app.get('/api/:imageID/message.png', function(req, res){
+  // extract imageID from request
+  var imageID = req.params.imageID;
+  console.log('imageID',imageID);
+  // get dataURL string from firebase
+  var options = {
+    host: 'https://dynamicemail.firebaseio.com',
+    path: '/'+imageID+'/img.json',
+    method: 'GET',
+    headers: {
+      accept: '*/*'
     }
+  };
 
-    response.type = matches[1];
-    response.data = new Buffer(matches[2], 'base64');
-
-    return response;
-  }
-
-//3913092
-
-  var imageBuffer = decodeBase64Image(data);
-  fs.writeFile(pathToImage + '.jpg', imageBuffer.data, function(){
-    res.end(imageBuffer.data);
+  var req = http.request(options, function(firebaseRes){
+    var firebaseData = '';
+    firebaseRes.on('data', function (chunk) {
+      firebaseData += chunk;
+    });
+    firebaseRes.on('end', function () {
+      console.log('firebaseData',firebaseData);
+      // decode dataURL as image
+      var image = decodeBase64Image(firebaseRes);
+      console.log('image',image);
+      // respond with image
+      res.writeHead('200', {'Content-Type': 'image/png'});
+      res.end(image, 'binary');
+    });
   });
-});
-
-// change to app.put
-app.post('/api/update/:imgid', function(req, res){
-});
-
-// change to app.delete
-app.post('/api/delete/:imgid', function(req, res){
-});
-
-app.get('/api/images/:imgid', function(req, res){
-  var pathToImage = path.join(__dirname, '/images/', req.params.imgid + '.jpg');
-  console.log('image request for',pathToImage);
-  res.sendFile(pathToImage);  
 });
 
 var port = process.env.PORT || 8000;
 app.listen(port, function(){
   console.log('Listening on port',port);
 });
+
+function decodeBase64Image(dataString) {
+  var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+  var response = {};
+  if (matches.length !== 3) {
+    return new Error('Invalid input string');
+  }
+  response.type = matches[1];
+  response.data = new Buffer(matches[2], 'base64');
+  return response;
+};
